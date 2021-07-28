bb0acd5043cf9064c135667f7ae6542cad988c7f
xiaohei <2896240118@qq.com>
2021-07-28 14:40:27 +0800

    Initial Commit

---
+++ src/router.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    path: '/pages/index/main',
+    config: {
+      navigationBarTitleText: '提现'
+    }
+  },
+  {
+    path: '/pages/logs/main',
+    config: {
+      navigationBarTitleText: '提现记录'
+    }
+  }
+]
